06418b9af29334f4a3dffc8759eddd39a0116c85
srtalaie <srt4721@yahoo.com>
2023-03-20 15:35:08 -0700

    10.8

--- src/components/Sigin.jsx
+++ src/components/Sigin.jsx
@@ -1,7 +1,24 @@
+import { Formik } from 'formik';
+import { Button, View } from 'react-native';
+
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik>
+      <View>
+        <Text>Sign in</Text>
+        <FormikTextInput name="username" placeholder="username" />
+        <FormikTextInput name="password" placeholder="password" secureTextEntry />
+        <Button onPress={onSubmit}>Sign in</Button>
+      </View>
+    </Formik>
+  )
 };
 
 export default SignIn;
